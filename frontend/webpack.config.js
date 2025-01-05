9cce8b1565883977d5718db131ca1a11c2e0470b
naldihamza16 <naldihamza16@gmail.com>
2025-01-05 19:56:01 +0100

    Save local changes before merge

--- frontend/webpack.config.js
+++
@@ -1,9 +0,0 @@
-// webpack.config.js
-import Dotenv from 'dotenv-webpack';
-
-export default {
-  // Your other Webpack configurations (entry, output, etc.)
-  plugins: [
-    new Dotenv()  // This will load the .env file
-  ],
-};
